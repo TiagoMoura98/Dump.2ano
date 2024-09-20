5acd56dbdeca38d37b0e94b75816e24e79a8d3f2
Tiago Moura <1160715@isep.ipp.pt>
2024-09-20 18:34:11 +0100

    initial commit with different folders
    Added folders for:
    
    ESINF
    BDDAD
    FSIAP
    ARQCP

---
+++ ESINF/ESINF.Treino/src/Main.java
@@ -0,0 +1,54 @@
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println(inverte("abcdefg"));
+
+        System.out.println(inverte_tail("qwerty", ""));
+
+        System.out.println(prod(10,4,0));
+
+    }
+
+    public static String inverte(String str) {
+
+        if (str.isEmpty()) {
+            return "";
+        }
+        return "" + inverte(str.substring(1)) + str.charAt(0);
+    }
+
+    public static String inverte_tail(String str, String res) {
+
+        if (str.isEmpty()) {
+            return res;
+        }
+
+        res = str.charAt(0) + res;
+        return inverte_tail(str.substring(1), res);
+    }
+
+    public static int prod (int m, int n, int res){
+
+        if (n == 0){
+            return res;
+        }
+
+        res += m;
+        n --;
+        return (prod(m,n,res));
+    }
+
+    public static int sum_a (int [] a){
+        return sum_a_helper (a, a.length - 1);
+    }
+
+    public static int sum_a_helper (int [] a, int index){
+        if(index < 0){
+            return 0;
+        }
+
+        return a[index] + sum_a_helper(a,index - 1);
+    }
+
+
+}
